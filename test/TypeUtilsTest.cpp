c1725cc8d13d629b6ca758304a9f449f53aa883c
fanchao <me@fanchao.nz>
2020-08-09 11:51:38 +1200

    Added catch2

--- test/TypeUtilsTest.cpp
+++ test/TypeUtilsTest.cpp
@@ -2,11 +2,14 @@
 // Created by Fanchao Liu on 8/08/20.
 //
 
-#include <QtTest/QtTest>
+#include <catch2/catch.hpp>
 
+#include <QObject>
+
+#include <optional>
 #include "TypeUtils.h"
 
-class TypeUtilsTest : public QObject {
+class TestClass : public QObject {
 Q_OBJECT
 public:
     enum TestEnum {
@@ -14,44 +17,32 @@ public:
     };
 
     Q_ENUM(TestEnum);
-
-private slots:
-
-    void testEnumToString_data() {
-        QTest::addColumn<TestEnum>("input");
-        QTest::addColumn<QString>("output");
-
-        QTest::newRow("E1") << E1 << "E1";
-        QTest::newRow("E2") << E2 << "E2";
-        QTest::newRow("E3") << E3 << "E3";
-        QTest::newRow("No such element") << static_cast<TestEnum>(E3 + 1) << "";
-    };
-
-    void testEnumToString() {
-        QFETCH(TestEnum, input);
-        QFETCH(QString, output);
-        QCOMPARE(sqlx::enumToString<TestEnum>(input), output);
-    };
-
-    void testEnumFromString_data() {
-        QTest::addColumn<QString>("input");
-        QTest::addColumn<QVariant>("output");
-
-        QTest::newRow("E1") << QStringLiteral("E1") << QVariant::fromValue(E1);
-        QTest::newRow("E2") << QStringLiteral("E2") << QVariant::fromValue(E2);
-        QTest::newRow("E3") << QStringLiteral("E3") << QVariant::fromValue(E3);
-        QTest::newRow("No such element") << "nothing" << QVariant();
-    }
-
-    void testEnumFromString() {
-        QFETCH(QString, input);
-        QFETCH(QVariant, output);
-
-        auto actual = sqlx::enumFromString<TestEnum>(input.toUtf8().data());
-        QCOMPARE(actual ? QVariant::fromValue(*actual) : QVariant(), output);
-    }
 };
 
-QTEST_MAIN(TypeUtilsTest)
+
+TEST_CASE("Enum to string") {
+    auto[input, output] = GENERATE(table<TestClass::TestEnum, QString>(
+            {
+                    {TestClass::E1, QStringLiteral("E1")},
+                    {TestClass::E2, QStringLiteral("E2")},
+                    {TestClass::E3, QStringLiteral("E3")},
+                    {static_cast<TestClass::TestEnum>(5), ""},
+            }));
+
+    REQUIRE(sqlx::enumToString(input) == output);
+}
+
+TEST_CASE("Enum from string") {
+    auto[input, expected] = GENERATE(table<const char*, std::optional<TestClass::TestEnum>>(
+            {
+                    { "e1", std::nullopt },
+                    { "E1", TestClass::E1 },
+                    { "E2", TestClass::E2 },
+                    { "E3", TestClass::E3 },
+                    { "E4", std::nullopt},
+            }));
+
+    CHECK(sqlx::enumFromString<TestClass::TestEnum>(input) == expected);
+}
 
 #include "TypeUtilsTest.moc"
